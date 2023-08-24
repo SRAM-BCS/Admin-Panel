a45954bd6e188674b985139f96110bbae74a4c2c
Debajyoti111 <debajyotidhar111@gmail.com>
2023-08-25 02:08:34 +0530

    add modal

--- src/Components/FCBCard.tsx
+++ src/Components/FCBCard.tsx
@@ -3,6 +3,7 @@ import { DefaultFCBCardItems } from "./DefaultCardItems";
 import { Card } from "flowbite-react";
 
 const FCBCard = ({ details = DefaultFCBCardItems[0] }) => {
+  const [isOpen, setIsOpen] = React.useState(false);
   return (
     <Card>
       <div className="text-left">
---
+++ src/Components/FacultySubject.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import Modal from "react-modal";
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+  },
+};
+
+const FacultySubject = ({ setIsOpen, isOpen, faculty }: any) => {
+  const openModal = () => {
+    setIsOpen(true);
+  };
+  const closeModal = () => {
+    setIsOpen(false);
+  };
+
+  const afterOpenModal = () => {
+    // you modal open logic here
+  };
+  return (
+    <div>
+      <Modal
+        isOpen={isOpen}
+        onAfterOpen={afterOpenModal}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Example Modal"
+      >
+        <h2>Hello</h2>
+        <button onClick={closeModal}>close</button>
+        <div>I am a modal</div>
+        <form>
+          <input />
+          <button>tab navigation</button>
+          <button>stays</button>
+          <button>inside</button>
+          <button>the modal</button>
+        </form>
+      </Modal>
+    </div>
+  );
+};
+
+export default FacultySubject;
